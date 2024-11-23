6c05f6a5276f118c414e1b0ce38e04f7d9743645
Phuc Nguyen <phucnguyen@MBP-cua-Phuc.lan>
2024-11-23 16:42:27 +0700

    day code

--- fe-core/components/LayoutBase/index.tsx
+++ fe-core/components/LayoutBase/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-configResponsive({ sm: 640 });
+configResponsive({ sm: 640, md: 800 });
 
 import { useEffect, useLayoutEffect, useMemo, useState } from "react";
 import { useDispatch } from "react-redux";
