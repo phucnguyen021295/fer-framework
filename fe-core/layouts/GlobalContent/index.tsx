708505436b889ffe4004fbba28b62c89a3f9616b
Phuc Nguyen <phucnguyen@MacBook-Pro-cua-Phuc.local>
2025-02-24 23:28:42 +0700

    day code

--- fe-core/layouts/GlobalContent/index.tsx
+++ fe-core/layouts/GlobalContent/index.tsx
@@ -1,12 +1,13 @@
 "use client";
 import { Flex } from "antd";
 import useResponsivePadding from "@/fe-core/hooks/useResponsivePadding";
+import { memo } from "react";
 
 interface Props {
   children: React.ReactNode;
 }
 
-const GlobalContent: FC<Props> = (props: Props) => {
+const GlobalContent = (props: Props) => {
   const padding = useResponsivePadding();
   const { children } = props;
 
